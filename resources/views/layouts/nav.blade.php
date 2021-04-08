9e719ff9f0ce4a7a6a407bc629df1d9e14d72f9c
Mumtoz Kodirov <imumtoz@gmail.com>
2021-04-08 17:21:51 +0500

    Added top menu link for browsing nodes.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,6 +7,13 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('nodes.index') }}">{{ __('Browse Nodes') }}</a>
+            </li>
+        </ul>
+
         <!-- Right Side Of Navbar -->
         <ul class="navbar-nav ml-auto">
             <!-- Authentication Links -->
