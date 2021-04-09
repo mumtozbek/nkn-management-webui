201eacda59231562e07c62d3aea1837a0484f416
Mumtoz Kodirov <imumtoz@gmail.com>
2021-04-09 17:23:30 +0500

    Uptime tracking frequency changed to every 10 minutes.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('uptime:refresh')->everyFiveMinutes();
+        $schedule->command('uptime:refresh')->everyTenMinutes();
     }
 
     /**
