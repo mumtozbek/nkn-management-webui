91a20b56e7bce0a85823f87d8dd05e91aad5b2aa
Mumtoz Kodirov <imumtoz@gmail.com>
2021-04-10 11:22:12 +0500

    Added flash messages to listing pages.

--- resources/views/nodes/index.blade.php
+++ resources/views/nodes/index.blade.php
@@ -8,6 +8,13 @@
                     {{ __('Monitoring Nodes') }}
                 </h2>
 
+                @if(session()->has('flash'))
+                    <div class="alert alert-success">
+                        {{ session()->get('flash') }}
+                        <button type="button" class="close" data-dismiss="alert">x</button>
+                    </div>
+                @endif
+
                 {{ $dataTable->table([], false, false) }}
             </div>
         </div>
