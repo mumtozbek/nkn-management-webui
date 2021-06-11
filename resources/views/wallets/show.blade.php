b84640ebc13500dfe583f4a672b348555787296f
Mumtoz Kodirov <imumtoz@gmail.com>
2021-06-11 14:00:12 +0500

    wallet show form fixed

--- resources/views/wallets/show.blade.php
+++ resources/views/wallets/show.blade.php
@@ -6,33 +6,35 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        {{ __('Edit the Account') }}
+                        {{ __('Show the Wallet') }}
                     </div>
 
                     <div class="card-body">
                         <div class="form-group">
-                            <label for="host">{{ __('Host') }}</label>
-                            <input type="text" name="host" id="host" value="{{ $account->host }}" class="form-control" readonly>
+                            <label for="name">{{ __('Node') }}</label>
+                            <input type="text" name="node" id="node" value="{{ $wallet->node->host }}" class="form-control" readonly>
                         </div>
 
                         <div class="form-group">
-                            <label for="host">{{ __('Provider') }}</label>
-                            <input type="text" name="provider" id="provider" value="{{ $account->provider->name }}" class="form-control" readonly>
+                            <label for="name">{{ __('Address') }}</label>
+                            <input type="text" name="address" id="address" value="{{ $wallet->address }}" class="form-control" readonly>
                         </div>
 
                         <div class="form-group">
-                            <a href="{{ route('accounts.index') }}" class="btn btn-secondary">
+                            <label for="keystore">{{ __('Keystore') }}</label>
+                            <textarea name="keystore" id="keystore" class="form-control" rows="10" readonly>{{ $wallet->keystore }}</textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">{{ __('Password') }}</label>
+                            <input type="text" name="password" id="password" value="{{ $wallet->password }}" class="form-control" readonly>
+                        </div>
+
+                        <div class="form-group">
+                            <a href="{{ route('wallets.index') }}" class="btn btn-secondary">
                                 {{ __('Back to list') }}
                             </a>
                         </div>
-
-                        @if(count($errors))
-                            <ul class="alert alert-danger">
-                                @foreach($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
                     </div>
                 </div>
             </div>
