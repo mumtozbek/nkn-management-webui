3a16594a901ca22f19f283be498fccbdffd156f7
Mumtoz Kodirov <imumtoz@gmail.com>
2021-06-12 18:32:28 +0500

    1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,17 +9,23 @@
 
     <title>{{ config('app.name') }}</title>
 
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Font awesome -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+
+    <!-- DevExtreme theme -->
+    <link href="https://cdn3.devexpress.com/jslib/21.1.3/css/dx.light.css" rel="stylesheet">
+
+    <!-- ChartJS library -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.0.2/chart.min.js"></script>
 </head>
 <body>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.0.2/chart.min.js"></script>
-
     <div id="app">
         <nav id="header-nav" class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             @include('layouts.nav')
@@ -30,7 +36,11 @@
     </div>
 
     <script src="{{ mix('js/app.js') }}"></script>
-    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
+
+    <!-- DevExtreme library -->
+    <script src="https://cdn3.devexpress.com/jslib/21.1.3/js/dx.all.js"></script>
+    <script src="https://unpkg.com/devextreme-aspnet-data@2.8.2/js/dx.aspnet.data.js"></script>
+
     @stack('scripts')
 </body>
 </html>
